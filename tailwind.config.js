3cfb1924941a02e5441faebd7b2bd3c841ed6735
Vinicius Macedo <vinicius.silva1076@etec.sp.gov.br>
2023-09-30 20:37:20 -0300

    m

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: "Poppins",
+      sans: "Open Sans",
+    },
+    colors: {
+      dark: "#0e122b",
+      blue: "#4a84e8",
+      white: "#fff",
+      black: "#111"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
